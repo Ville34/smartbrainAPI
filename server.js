2dede98e50e6ebbd0e2d719082c8c4c2afe7a55c
Ville Suutari <suutari.ville@gmail.com>
2022-06-02 20:49:16 +0300

    changed server host

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-opaque-10012',
     user : 'ville',
-    password : '9yBeHarE',
+    password : '',
     database : 'smartbrain'
   }
 });
